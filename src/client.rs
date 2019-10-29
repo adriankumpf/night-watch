2056961f6a3925ad73d4cde15f9d0a355e7f8928
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-10-29 23:55:42 +0100

    Rewrite "sun-automaton" in Rust!

--- src/client.rs
+++ src/client.rs
@@ -1,22 +1,35 @@
-use chrono::prelude::*;
 use anyhow::Result;
+use chrono::prelude::*;
+use image::RgbImage;
 use reqwest::{header, Url};
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize, Clone)]
-pub struct Entity<T> {
-    attributes: T,
-    last_changed: DateTime<Utc>,
-    last_updated: DateTime<Utc>,
-    state: String,
+#[derive(Debug, Deserialize)]
+pub struct Entity<T, S> {
+    pub attributes: T,
+    pub last_changed: DateTime<Utc>,
+    pub last_updated: DateTime<Utc>,
+    pub state: S,
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Deserialize)]
+pub struct EventResult {
+    pub message: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum State {
+    BelowHorizon,
+    AboveHorizon,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Sun {
-    azimuth: f32,
-    elevation: f32,
-    next_rising: DateTime<Utc>,
-    next_setting: DateTime<Utc>,
+    pub next_rising: DateTime<Utc>,
+    pub next_setting: DateTime<Utc>,
+    pub next_dawn: DateTime<Utc>,
+    pub next_dusk: DateTime<Utc>,
 }
 
 #[derive(Clone)]
@@ -40,9 +53,52 @@ impl Client {
         Ok(Client { client, base })
     }
 
-    pub async fn get_sun<T>(&self, entitiy_id: &str) -> Result<Entity<Sun>> {
-        let url = self.base.join(&format!("/api/states/{}", entitiy_id))?;
-        let state = self.client.get(url).send().await?.json().await?;
+    pub async fn get_sun(&self) -> Result<Entity<Sun, State>> {
+        let url = self.base.join("/api/states/sun.sun")?;
+
+        let state = self
+            .client
+            .get(url)
+            .send()
+            .await?
+            .error_for_status()?
+            .json()
+            .await?;
+
         Ok(state)
+    }
+
+    pub async fn get_image(&self, camera: &str) -> Result<RgbImage> {
+        let url = self
+            .base
+            .join(&format!("/api/camera_proxy/camera.{}", camera))?;
+
+        let bytes = self
+            .client
+            .get(url)
+            .send()
+            .await?
+            .error_for_status()?
+            .bytes()
+            .await?;
+
+        let image = image::load_from_memory(&bytes)?.to_rgb();
+
+        Ok(image)
+    }
+
+    pub async fn post_event(&self, event: &str) -> Result<EventResult> {
+        let url = self.base.join(&format!("/api/events/{}", event))?;
+
+        let result = self
+            .client
+            .post(url)
+            .send()
+            .await?
+            .error_for_status()?
+            .json()
+            .await?;
+
+        Ok(result)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use structopt::StructOpt;
+mod client;
 
 use anyhow::Result;
+use chrono::prelude::*;
+use image::{Pixel, RgbImage};
 use reqwest::Url;
+use structopt::StructOpt;
+use tokio::timer;
 
-mod client;
+use client::{Client, Entity, EventResult, State, Sun};
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "sun-events")]
@@ -17,16 +21,93 @@ struct Args {
     url: Url,
 
     /// The access token for HA
-    #[structopt(short, long)]
+    #[structopt(short, long, env = "TOKEN")]
     token: String,
+
+    /// The close event
+    #[structopt(long, default_value = "close_rollershutters")]
+    close_event: String,
+
+    /// The open event
+    #[structopt(long, default_value = "open_rollershutters")]
+    open_event: String,
+}
+
+fn is_grayscale(image: RgbImage) -> bool {
+    let mut diff = 0;
+
+    for p in image.pixels() {
+        let channels = p.channels();
+        let (r, g, b) = (channels[0], channels[1], channels[2]);
+
+        let rg = ((r as i32) - (g as i32)).abs() as u32;
+        let rb = ((r as i32) - (b as i32)).abs() as u32;
+        let gb = ((g as i32) - (b as i32)).abs() as u32;
+
+        diff += rg + rb + gb;
+    }
+
+    let f = (diff as f64) / (image.width() * image.height()) as f64 / (255.0 * 3.0);
+    let is_grayscale = f < 0.001;
+
+    println!("{} – is_grayscale: {}", f, is_grayscale);
+
+    is_grayscale
+}
+
+async fn fetch_sun(client: &Client) -> Entity<Sun, State> {
+    let mut i = 0;
+
+    loop {
+        match client.get_sun().await {
+            Err(_e) => {
+                let s = 2u64.pow(i);
+                println!("Home Assistant is not available. Retrying in {}s", s);
+                timer::delay_for(std::time::Duration::from_secs(s)).await;
+                i += 1;
+            },
+            Ok(sun) => return sun,
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::from_args();
 
-    let client = client::Client::new(args.url, &args.token)?;
-    dbg!(client.get_sun::<client::Sun>("sun.sun").await?);
+    let client = Client::new(args.url, &args.token)?;
 
-    Ok(())
+    loop {
+        let sun = fetch_sun(&client).await;
+
+        let next_event = std::cmp::min(sun.attributes.next_dawn, sun.attributes.next_dusk);
+
+        let (kind, time, event) = match sun.state {
+            State::BelowHorizon => ("Sunrise", sun.attributes.next_rising, &args.open_event),
+            State::AboveHorizon => ("Sunset", sun.attributes.next_setting, &args.close_event),
+        };
+
+        let sleep_for_hours = (time - Utc::now()).num_minutes() as f32 / 60.0;
+        println!("Next {} in {:.1} hours", kind, sleep_for_hours);
+
+        timer::delay_for((next_event - Utc::now()).to_std()?).await;
+
+        println!("{} in {} minutes", kind, (time - Utc::now()).num_minutes());
+
+        loop {
+            let image = client.get_image(&args.camera).await?;
+
+            if match sun.state {
+                State::BelowHorizon => !is_grayscale(image),
+                State::AboveHorizon => is_grayscale(image),
+            } {
+                break;
+            }
+
+            timer::delay_for(std::time::Duration::from_secs(10)).await;
+        }
+
+        let EventResult { message } = client.post_event(&event).await?;
+        println!("{} [{:+}]", message, (Utc::now() - time).num_minutes());
+    }
 }
